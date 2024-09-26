dd2188358654af6ef0aeece5fb2f3a2f162a6f02
Ayman Farhat <aymanf@google.com>
2024-09-26 23:26:24 +0200

    Add pinging HTTP endpoint example + timing

--- src/calc_primes/main.py
+++ src/calc_primes/main.py
@@ -26,7 +26,7 @@ def check_prime_numbers(arr: List[int]):
     return [is_prime(elem) for elem in arr]
 
 
-def check_prime_numbers_multiprocessing(arr: List[int], cores: int = 4):
+def check_prime_numbers_multiprocessing(arr: List[int], cores: int = os.cpu_count()):
     """Check if the numbers in the array are prime or not, runs using multiprocessing"""
     from multiprocessing import Pool
 
@@ -44,7 +44,7 @@ def check_prime_numbers_multiprocessing(arr: List[int], cores: int = 4):
     return results
 
 
-def check_prime_numbers_multithreading(arr: List[int], threads: int = 4):
+def check_prime_numbers_multithreading(arr: List[int], threads: int = 8):
     """Check if the numbers in the array are prime or not, runs using multithreading"""
     from threading import Thread
 
@@ -62,32 +62,39 @@ def check_prime_numbers_multithreading(arr: List[int], threads: int = 4):
         def run(self):
             self.value = check_prime_numbers(self.chunk)
 
+    threads = []
     for chunk in chunks:
         thread = PrimeNumberThread(chunk)
         thread.start()
+        threads.append(thread)
+
+    for thread in threads:
         thread.join()
-        results.append(thread.value)
+        results.extend(thread.value)
 
     return results
 
 
 @click.command()
-@click.option("--limit", default=400, help="Number of elements in the array")
+@click.option("--limit", default=60000, help="Number of elements in the array")
 @click.option("--mode", default="sequential", help="Mode of execution")
 def run(limit: int, mode: str):
     # Generate random data
     num_arr = generate_data(limit)
 
+    import time
+    start = time.time()
     match mode:
         case "sequential":
-            print(check_prime_numbers(num_arr))
+            check_prime_numbers(num_arr)
         case "multithreading":
-            print(check_prime_numbers_multithreading(num_arr))
+            check_prime_numbers_multithreading(num_arr)
         case "multiprocessing":
-            print(check_prime_numbers_multiprocessing(num_arr))
+            check_prime_numbers_multiprocessing(num_arr)
         case _:
             raise ValueError("Invalid mode provided")
 
+    print(f"Time taken: {time.time() - start}")
 
 if __name__ == "__main__":
     run()
--- src/http_extraction/main.py
+++ src/http_extraction/main.py
@@ -1,5 +1,6 @@
 import os
 import logging
+from itertools import batched
 from dotenv import load_dotenv
 import click
 from requests import get
@@ -7,28 +8,85 @@ from requests import get
 logging.basicConfig(level=logging.INFO)
 load_dotenv()
 
-def ping_service_seq(urls: list[str]) -> list[str]:
+def ping_service_seq(urls: list[str], thread_name:str="") -> list[str]:
     """Ping the service sequentially"""
     responses = []
 
     for url in urls:
         response = get(url)
-        logging.info(f"Response: {response.text}")
+        logging.info(f"Response: {response.text} via {thread_name}")
         responses.append(response.text)
 
     return responses
 
+def ping_service_multiprocessing(urls: list[str], cores:int) -> list[str]:
+    """Ping the service using multiprocessing"""
+    from multiprocessing import Pool
+
+    chunk_size = len(urls) // cores
+    chunks = batched(urls, chunk_size)
+
+    results = []
+
+    with Pool(processes=cores) as pool:
+        process_pool = pool.imap(ping_service_seq, chunks)
+
+        for result in process_pool:
+            results.extend(result)
+
+    return results
+
+def ping_service_multithreading(urls: list[str], threads:int) -> list[str]:
+    """Ping the service using multithreading"""
+    from threading import Thread
+
+    chunk_size = len(urls) // threads
+    chunks = batched(urls, chunk_size)
+
+    results = []
+
+    class PingThread(Thread):
+        def __init__(self, chunk):
+            super().__init__()
+            self.chunk = chunk
+            self.value = []
+
+        def run(self):
+            self.value = ping_service_seq(self.chunk, self.name)
+
+    threads = []
+    for chunk in chunks:
+        thread = PingThread(chunk)
+        thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
+        results.extend(thread.value)
+
+    return results
+
 @click.command()
-@click.option("--limit", default=20, help="Number of elements in the array")
+@click.option("--limit", default=400, help="Number of elements in the array")
 @click.option("--mode", default="sequential", help="Mode of execution")
 def run(limit: int, mode: str):
     urls = [os.getenv("HTTP_ENDPOINT")] * limit
 
+    import time
+    start = time.time()
     match mode:
         case "sequential":
             print(ping_service_seq(urls))
+        case "multithreading":
+            print(ping_service_multithreading(urls, 20))
+        case "multiprocessing":
+            cores = os.cpu_count()
+            print(ping_service_multiprocessing(urls, cores))
         case _:
             raise ValueError("Invalid mode provided")
 
+    print(f"Total running time: {time.time() - start}")
+
 if __name__ == "__main__":
+    # Measure the time taken to run the program
     run()
